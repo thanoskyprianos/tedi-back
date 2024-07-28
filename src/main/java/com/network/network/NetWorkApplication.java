56ab17e15562e57545130704a44bfbb5e433ccfb
Thanasis Kyprianos <than20022004@gmail.com>
2024-07-28 19:24:09 +0300

    Initial Commit

---
+++ src/main/java/com/network/network/NetWorkApplication.java
@@ -0,0 +1,13 @@
+package com.network.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetWorkApplication.class, args);
+    }
+
+}
